be65dfbafd26d069fbea064175349a58025df921
Study Mac <janetbrennan@janetbrnansimac.home>
2016-02-22 01:30:11 +0000

    Cleaning up calculateOverpasses.cpp

--- calculateOverpasses.cpp
+++ calculateOverpasses.cpp
@@ -4,86 +4,24 @@
 
     Takes an of locations csv and swath csv
 */
+#define _USE_MATH_DEFINES
 
 #include <iostream>
 #include <iterator>
 #include <sstream>
 #include <vector>
 #include <fstream>
-
-#define _USE_MATH_DEFINES
- 
 #include <cmath>
 
 using namespace std;
 
-
-const int swathRows = 20554;
-const int swathCols = 5;
+// Define some consts
 const double R_earth = 6378137.0; // earth radius [m]
-
-//void load_swath_file(string filename, )
-
-/*
-    File loading stuff
-*/
-vector< vector<double> > load_swath_file(string filename) 
-{
-    // Set up storage vector
-    vector< vector<double> > swStore;
-    vector<double> swRow;
-
-    ifstream stream(filename);
-    
-    string line;
-    string item;
-
-    for (string line; getline(stream, line); )
-    {
-        istringstream ln(line);
-        while (getline(ln, item, ','))
-        {
-            swRow.push_back(atof(item.c_str()));
-        }
-        swStore.push_back(swRow);
-        swRow.clear();
-    }
-
-    return swStore;
-}
-
-
-
-vector< vector<double> > load_pixels_file(string filename) 
-{
-    // Set up storage vector
-    vector< vector<double> > swStore;
-    vector<double> swRow;
-
-    ifstream stream(filename);
-    
-    string line;
-    string item;
-
-    for (string line; getline(stream, line); ) // get line in file
-    {
-        istringstream ln(line);
-        while (getline(ln, item, ',')) // get field in line
-        {
-            swRow.push_back(atof(item.c_str())); // load value 
-        }
-        swStore.push_back(swRow);
-        swRow.clear();
-    }
-
-    return swStore;
-}
 
 /*
     Store the swaths in a more convenient structure..
-
 */
-struct swath
+struct swath 
 {
     int orbit_id;
     vector<long> utc;
@@ -91,7 +29,6 @@ struct swath
     vector<double> grLatitude;
     vector<double> swWidth;
 };
-
 
 vector<swath>  convert_to_swaths(vector< vector<double> >  &swaths2D) {
     /*
@@ -105,35 +42,32 @@ vector<swath>  convert_to_swaths(vector< vector<double> >  &swaths2D) {
     /*
     1. Count number of seperate swaths and store lenghts of each...
     */
-    
     std::vector<swath> allSwaths;
 
     int id = 0;
     std::vector<int> id_counts;
     int entry_id=0;
     int id_number_elements = 0;
+    // loop over each swath
     for (int i=0; i < swaths2D.size(); i++) {
         id_number_elements++;
         entry_id = swaths2D[i][3];
         if (id != entry_id) 
         {
-            
             id++;
             id_counts.push_back(id_number_elements);
             // reset to zero
-            id_number_elements =0;
+            id_number_elements = 0;
         }
     }
     int offset = 0;
-    for (int i=0; i<id; i++) 
-    {
-        
+    // make swaths
+    for (int i=0; i<id; i++) {
         /*
         Create a swath...
         */
         swath aSwath = swath();
         aSwath.orbit_id = i;
-        // cout << "Line 139" << endl;
         /*
         Collect the data...
         */ 
@@ -142,15 +76,12 @@ vector<swath>  convert_to_swaths(vector< vector<double> >  &swaths2D) {
         double Latitude;
         double swathWidth;
         int number_of_entries = id_counts[i];
-        for (int rec=0; rec < number_of_entries; rec++)
-        {
+        for (int rec=0; rec < number_of_entries; rec++) {
             // collect entries
-            // utc
             utc = swaths2D[offset][0];
             Latitude = swaths2D[offset][1];
             Longitude = swaths2D[offset][2];
             swathWidth = swaths2D[offset][4];
-            //cout << offset + rec << endl;
             // add to the struct vectors...
             aSwath.utc.push_back(utc);
             aSwath.grLatitude.push_back(Latitude);
@@ -159,8 +90,6 @@ vector<swath>  convert_to_swaths(vector< vector<double> >  &swaths2D) {
             // add one to offset
             offset++;
         }
-        // increase offset by the number of elments -1
-        //offset += (number_of_entries -1);
         // add this swath to the swaths vec
         allSwaths.push_back(aSwath);
     }
@@ -169,9 +98,63 @@ vector<swath>  convert_to_swaths(vector< vector<double> >  &swaths2D) {
 
 
 
+/*
+    File loading stuff
+    ==================
+*/
+vector< vector<double> > load_swath_file(string filename) {
+    // Set up storage vector
+    vector< vector<double> > swStore;
+    vector<double> swRow;
+    // load file
+    ifstream stream(filename);
+    // file elements
+    string line;
+    string item;
+    // loop over lines
+    for (string line; getline(stream, line); )
+    {
+        istringstream ln(line);
+        while (getline(ln, item, ','))
+        {
+            // get item in row
+            swRow.push_back(atof(item.c_str()));
+        }
+        swStore.push_back(swRow);
+        swRow.clear();
+    }
+    return swStore;
+}
+
+vector< vector<double> > load_pixels_file(string filename) 
+{
+    // Set up storage vector
+    vector< vector<double> > swStore;
+    vector<double> swRow;
+    // load file
+    ifstream stream(filename);
+    // file elements
+    string line;
+    string item;
+    // loop over lines
+    for (string line; getline(stream, line); ) 
+    {
+        istringstream ln(line);
+        while (getline(ln, item, ','))
+         { 
+            // get field in line
+            swRow.push_back(atof(item.c_str())); 
+        }
+        swStore.push_back(swRow);
+        swRow.clear();
+    }
+    return swStore;
+}
+
 
 /*
     Geodetic
+    ========
 */
 
 double haversine(double lat1, double lon1, double lat2, double lon2) {
@@ -184,9 +167,6 @@ double haversine(double lat1, double lon1, double lat2, double lon2) {
     lat1 = lat1 * (M_PI / 180.0); 
     lon2 = lon2 * (M_PI / 180.0); 
     lat2 = lat2 * (M_PI / 180.0); 
-
-    //double r = 6378137.0 // earth radius [m]
-
     // haversine formula 
     double dlon = lon2 - lon1;
     double dlat = lat2 - lat1;
@@ -195,12 +175,6 @@ double haversine(double lat1, double lon1, double lat2, double lon2) {
     double dist = c * R_earth;
     return dist;
 }
-
-
-
-/*
-    method
-*/
 
 double latitudeDistance(double lat0, double lat1) {
     // compute distance between two latitudes
@@ -211,8 +185,7 @@ double latitudeDistance(double lat0, double lat1) {
 }
 
 
-int count_overpasses(double lat, double lon, std::vector<swath> &swaths )
-{
+int count_overpasses(double lat, double lon, std::vector<swath> &swaths ) {
     /*
         Calculate the number of overpasses for a location
     */
@@ -223,27 +196,21 @@ int count_overpasses(double lat, double lon, std::vector<swath> &swaths )
         2. Whether the coord at [1.] is within 0.5 * swathWidth or this earthPoint.
     */
 
-
     std::vector<swath>::iterator sw;
     for (sw = swaths.begin(); sw != swaths.end(); ++sw) {
-        //cout << "Swath id is: " << sw->orbit_id << endl;
         /*
         Iterate through all the latitudes of the swath to find the nearest
         */
         int min_latitude_idx = 0;
         double dist = 0;
         double mindist = 1e12;
-
-
         int idx = 0;
+        // loop over all latitudes in swath
         for (int j=0; j <  sw->grLatitude.size(); j++)
         {
-
             /*
-                Find index of nearest latitude
-                
+                Find index of nearest latitude 
             */
-            // cout << orbitLat << endl;
             double orbitLat = sw->grLatitude[j];
             dist = latitudeDistance( lat, orbitLat );
             //cout << dist << endl;
@@ -252,13 +219,10 @@ int count_overpasses(double lat, double lon, std::vector<swath> &swaths )
                 // set min distance and set right latitude index...
                 mindist = dist;
                 min_latitude_idx = idx;
-                //cout << "++ "<< sw->orbit_id << " " <<  lat << " " << orbitLat << " "<< dist <<" "<< mindist <<" "<< idx <<" "<< min_latitude_idx  << endl;
-
             }
             // increment index counter
             idx++;
         }
-        //cout << "minimum distance is: "<< mindist << endl;
         /*
         Now have the minimum distance latitude location for this orbit
         Want to check whether this location to our point is within 0.5* swathWidth
@@ -266,7 +230,6 @@ int count_overpasses(double lat, double lon, std::vector<swath> &swaths )
         //cout << "min index is: " << min_latitude_idx << endl;
         double orbitLatitude =  sw->grLatitude[min_latitude_idx];
         double orbitLongitude = sw->grLongitude[min_latitude_idx];
-
         /*
         Perform distance calculation
         */
@@ -278,26 +241,26 @@ int count_overpasses(double lat, double lon, std::vector<swath> &swaths )
         Check whether this distance is less than 0.5 swath
         */
         if (distance < 0.5 * sw->swWidth[min_latitude_idx]) {
+            // add one to count
             counts++;
         }
-
-
     }
-    // cout << lat << " " << lon <<  " "<<counts << endl;
     return counts;
 }
 
+/*
+    Main
+    ====
+*/
 
-
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     
     if (argc != 3) {
         std::cerr << "* Count Overpasses *\n\n" 
                   << "Program which counts satellite overpasses for location.\n"
                   << "Outputs location and counts.\n\n"
-                  << "Usage: " << argv[0] << " [ ground_locations_file ]" 
-                  << " [ orbits_file ]"<< " > [ counts_file ] "<< std::endl;
+                  << "Usage: " << argv[0] << " [ orbits_file ]" 
+                  << " [ ground_locations_file ]"<< " > [ counts_file ] "<< std::endl;
         return(1);
     }
     // associate command line args with files
@@ -306,22 +269,12 @@ int main(int argc, char* argv[])
 
     // Load swath data file
     vector< vector<double> > swaths_f = load_swath_file(swathFile);
-    //cout << swaths_f[0][0] << endl;
-    //cout << swaths_f[1][1] << endl;
-    //cout << "HERE" << endl;
+
     // put into structures
-
-
-    vector<swath> swaths = convert_to_swaths(swaths_f); 
-    // cout << swaths[0].orbit_id << endl;
-    //cout << swaths[0].grLatitude[0] << endl;
-    //cout << swaths[1].grLongitude[0] << endl;
-    //int i = count_overpasses(lat, lon, swaths); 
+    vector<swath> swaths = convert_to_swaths(swaths_f);  
 
     // Load the pixel locations
     vector< vector<double> > locations = load_pixels_file(coordinates);
-    //cout << locations[0][0] << endl;
-    //cout << locations[1][1] << endl;
   
     /*
     Do counts
@@ -331,7 +284,6 @@ int main(int argc, char* argv[])
     double land;
     int count;
     double fill = -999.0;
-    //std::vector<int> counts;
     for (int i=0; i < locations.size(); i++) {
         /*
         Iterate through and do counts
@@ -342,13 +294,10 @@ int main(int argc, char* argv[])
         if (land == 1.0) {
             count =  count_overpasses(lat, lon, swaths);
             cout << lat << " " << lon <<  " "<<count << endl;
-            //counts.push_back(count); 
         }
         else {
             // is water so put a fill value
-            //counts.push_back(-999.0)
             cout << lat << " " << lon <<  " "<< fill << endl;
         }
-
     } 
 }
--- main.py
+++ main.py
@@ -22,31 +22,34 @@ def main():
         2. Append to shapefile
     3. Save shapefile
     """
-    name = "TERRA"
-    line1 = "1 25994U 99068A   16048.43680378  .00000258  00000-0  67198-4 0  9999"
-    line2 = "2 25994  98.1982 124.4247 0001352 105.3907 254.7441 14.57126067859938"
-    TERRA = ephem.readtle(name, line1, line2)
+    #name = "TERRA"
+    #line1 = "1 25994U 99068A   16048.43680378  .00000258  00000-0  67198-4 0  9999"
+    #line2 = "2 25994  98.1982 124.4247 0001352 105.3907 254.7441 14.57126067859938"
+    #TERRA = ephem.readtle(name, line1, line2)
+    
+
+    # Landsat 8
+    name = "Landsat8"
+    line1="1 39084U 13008A   16051.82349873  .00000188  00000-0  51829-4 0  9999"
+    line2="2 39084  98.1988 123.2603 0001265  89.4360 270.6984 14.57110027160810"
+    LD8 = ephem.readtle(name, line1, line2)
+    
+
     sun = ephem.Sun()
-    fov = np.radians(110)
+    fov = np.radians(68.6)
 
+    """
+    Make pandas dataframe to store swath information
+    """
+    import pandas as pd
+    data = {"DateTime": [],"orbit_id":[], "ground_lat": [], "ground_lon": [], "swath_width": []}
+    swaths = pd.DataFrame(data)
+    swaths.set_index(keys="DateTime")
     # generate shapefile
-    spatialReference = osr.SpatialReference()
-    # WGS84
-    spatialReference.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
 
-    # Now convert it to a shapefile with OGR
-    driver = ogr.GetDriverByName('Esri Shapefile')
-    ds = driver.CreateDataSource('orbits.shp')
-    layer = ds.CreateLayer('testing',  spatialReference, ogr.wkbMultiLineString)
-    # Add one attribute
-    layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
-    layer.CreateField(ogr.FieldDefn('swidth', ogr.OFTReal))
-    defn = layer.GetLayerDefn()
-
-    id_poly = 0
-
+    orbit_id = 0
     # need to do splitted by hemisphere unfortunately..
-    for orbit in make_an_orbit(TERRA):
+    for orbit in make_an_orbit(LD8):
         #import pdb; pdb.set_trace()
         if len(orbit) > 1:
             """
@@ -54,7 +57,8 @@ def main():
 
             """
             sun = ephem.Sun()
-            fov = np.radians(110)
+
+            print(orbit[0].datetime)
 
             for overpass in orbit:
                 overpass.only_daytime_overpasses(sun)
@@ -79,45 +83,39 @@ def main():
             """
             Pickle to figure this out
             """
-            from shapely.geometry import MultiLineString
-            import pickle
-            with open("orbit%i.pik" %id_poly , "wb") as f:
-                pickle.dump(orbit, f)
-            print "picked"
-            print orbit[0].true_heading
+            # from shapely.geometry import MultiLineString
+            # import pickle
+            # with open("orbit%i.pik" %id_poly , "wb") as f:
+            #     pickle.dump(orbit, f)
+            # print "picked"
+            # print orbit[0].true_heading
             #continue
             """
-            annyoing dateline wrap around means we need to check
-            whether an orbit crosses it
-            if it does we need to make two polygons
-
+            Create a tempoary dataframe for this orbit
             """
-            import splitting_geoms
-            try:
-                swath_polygon = make_two_swath_linestrings(orbit)
-            except ValueError: # not enough points for orbit...
-                continue
+            epoch = datetime.datetime(1970, 1, 1)
+            tmp_d = {"DateTime": [(o.datetime - epoch).total_seconds() for o in orbit],
+                     "orbit_id": orbit_id * np.ones(len(orbit)),
+                     "ground_lat": [o.lat for o in orbit],
+                     "ground_lon": [o.long for o in orbit],
+                     "swath_width": [o.swath_width for o in orbit]}
+            tmp = pd.DataFrame(tmp_d)
+            tmp.set_index(keys="DateTime")
+            #import pdb; pdb.set_trace()
+            orbit_id +=1 
+            """
+            Append to main dataframe
+            """
+            swaths = swaths.append(tmp)
+            #swaths.set_index(keys="DateTime")
 
-            line = MultiLineString([[[o.long, o.lat] for o in orbit]])
-
-
-            # add to the output shapefile
-            ## If there are multiple geometries, put the "for" loop here
-
-            # Create a new feature (attribute and geometry)
-            feat = ogr.Feature(defn)
-            feat.SetField('id', id_poly)
-            feat.SetField('swidth', 1.00)
-            id_poly +=1
-            # Make a geometry, from Shapely object
-            geom = ogr.CreateGeometryFromWkb(swath_polygon.wkb)
-            feat.SetGeometry(geom)
-
-            layer.CreateFeature(feat)
-            feat = geom = None  # destroy these
-
-    # Save shapefile
-    ds = layer = feat = geom = None
+    """
+    Save the DataFrame to a file
+    """
+    swaths = swaths.set_index(keys="DateTime")
+    #swaths.set_index(keys="DateTime")
+    #import pdb; pdb.set_trace()
+    swaths.to_csv("orbits.csv")
 
 
 if __name__ == "__main__":
